955726fcc9899c41cef5fa348392a81a427f0716
Ackon Samuel <ackons433@gmail.com>
2023-07-13 22:52:03 +0000

    Remove redundant imports

--- src/examples/LayoutContainers/DashboardLayout/index.js
+++ src/examples/LayoutContainers/DashboardLayout/index.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 
+import { useSelector, useDispatch } from "react-redux";
 // react-router-dom components
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 // prop-types is a library for typechecking of props.
 import PropTypes from "prop-types";
@@ -11,15 +12,40 @@ import MDBox from "components/MDBox";
 
 // Material Dashboard 2 React context
 import { useMaterialUIController, setLayout } from "context";
+import { getAdmin } from "redux/slices/admin/getAdminSlice";
+import { instrumentFetch } from "redux/slices/instruments/instrumentFetch";
+import { categoryFetch } from "redux/slices/categories/fetchCategorySlice";
+import { labFetch } from "redux/slices/labs/fetchLabSlice";
 import Copyright from "Copyright";
 
 function DashboardLayout({ children }) {
+  const categories = useSelector((state) => state.categoryFetch.category);
+  const labs = useSelector((state) => state.labFetch.lab);
+  const instruments = useSelector((state) => state.instrumentFetch.instrument);
   const [controller, dispatch] = useMaterialUIController();
   const { miniSidenav } = controller;
   const { pathname } = useLocation();
 
+  const dispatched = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     setLayout(dispatch, "dashboard");
+    dispatched(getAdmin()).then((res) => {
+      if (res.type !== "admin/getAdmin/fulfilled") {
+        navigate("/");
+      }
+      if (res.type === "admin/getAdmin/fulfilled") {
+        if (categories.length === 0) {
+          dispatched(categoryFetch());
+        }
+        if (labs.length === 0) {
+          dispatched(labFetch());
+        }
+        if (instruments.length === 0) {
+          dispatched(instrumentFetch());
+        }
+      }
+    });
   }, [pathname]);
 
   return (
