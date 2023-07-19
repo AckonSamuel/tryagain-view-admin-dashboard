b56409838af93d06a09daefbb48a8cf6a4501cdc
Ackon Samuel <ackons433@gmail.com>
2023-07-19 17:11:49 +0000

    hot fix

--- src/redux/common.js
+++ src/redux/common.js
@@ -1,3 +1,3 @@
-// const BASE_URL = "https://rails-1ubd.onrender.com";
-const BASE_URL = "http://127.0.0.1:3000";
+const BASE_URL = "https://rails-1ubd.onrender.com";
+// const BASE_URL = "http://127.0.0.1:3000";
 export default BASE_URL;
